ec31aec652959335419fe5cedc8fdc924ac5e8b9
outamghartAchraf <outamghart2000@gmail.com>
2026-02-26 12:38:30 +0000

    add function toggleFavorite

--- src/services/storageService.js
+++ src/services/storageService.js
@@ -4,3 +4,14 @@ export function getFavorites() {
   return JSON.parse(localStorage.getItem(KEY)) || [];
 }
 
+export function toggleFavorite(id) {
+  let favs = getFavorites();
+
+  if (favs.includes(id)) {
+    favs = favs.filter(f => f !== id);
+  } else {
+    favs.push(id);
+  }
+
+  localStorage.setItem(KEY, JSON.stringify(favs));
+}
