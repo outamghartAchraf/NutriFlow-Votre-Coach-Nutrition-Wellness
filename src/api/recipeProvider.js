85c4ca854ef5f63909e35205373c1123091a9b7b
outamghartAchraf <outamghart2000@gmail.com>
2026-02-26 12:22:32 +0000

    add code for function search API

--- src/api/recipeProvider.js
+++ src/api/recipeProvider.js
@@ -5,4 +5,9 @@ export async function getAllRecipes() {
   return data.recipes;
 }
 
-
+export async function searchRecipes(query) {
+  const res = await fetch(`https://dummyjson.com/recipes/search?q=${query}`);
+  if (!res.ok) throw new Error("Erreur Recherche");
+  const data = await res.json();
+  return data.recipes;
+}
